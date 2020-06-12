b02ed7a99c4832f841f3017353321641f1f3fef3
erekoba <erecasio0302@gmail.com>
2020-06-12 18:23:18 +0900

    first

---
+++ src/firebase/config.js
@@ -0,0 +1,11 @@
+// config.js
+export const firebaseConfig = {
+    apiKey: "AIzaSyDIlzJklmV29Wy9Y-wQscPplRkOPueEXa0",
+    authDomain: "react-firebase-aec43.firebaseapp.com",
+    databaseURL: "https://react-firebase-aec43.firebaseio.com",
+    projectId: "react-firebase-aec43",
+    storageBucket: "react-firebase-aec43.appspot.com",
+    messagingSenderId: "478268377491",
+    appId: "1:478268377491:web:d9de90a938ecc9c1ea0066",
+    measurementId: "G-SN16CQZSGT"
+};
